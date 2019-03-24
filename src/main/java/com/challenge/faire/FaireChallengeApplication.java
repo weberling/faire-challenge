fa3351a6a9abd123cec85cf24a04557b5a96e002
“weberling” <dea.weberling+git@gmail.com>
2019-03-24 09:18:20 -0300

    feat(accept-args)

--- src/main/java/com/challenge/faire/FaireChallengeApplication.java
+++ src/main/java/com/challenge/faire/FaireChallengeApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class FaireChallengeApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(FaireChallengeApplication.class, "--faire_token=HQLA9307HSLQYTC24PO2G0LITTIOHS2MJC8120PVZ83HJK4KACRZJL91QB7K01NWS2TUCFXGCHQ8HVED8WNZG0KS6XRNBFRNGY71");
+		ConfigurableApplicationContext context = SpringApplication.run(FaireChallengeApplication.class, args[0] != null ? args[0] : "--faire_token=HQLA9307HSLQYTC24PO2G0LITTIOHS2MJC8120PVZ83HJK4KACRZJL91QB7K01NWS2TUCFXGCHQ8HVED8WNZG0KS6XRNBFRNGY71");
 		context.getBean(Solution.class).execute();
 
 	}
